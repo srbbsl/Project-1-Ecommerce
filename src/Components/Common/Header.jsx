0657e8740ab80cf47d7964a0ee83175b8d06d230
srbbsl <saurabbsl@gmail.com>
2025-05-30 19:00:03 +0545

    changes

---
+++ src/Components/Common/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar';
+
+const Header = () => {
+  return (
+    <div>
+        {/* Topbar */}
+            <Topbar />
+        {/* Navbar */}
+        {/* Cart Drawer */}
+    </div>
+  )
+};
+
+export default Header;
